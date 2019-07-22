f21ea48ffb1f44dde644d4274b22c110eb6b746d
Valentin Lungu <schlumpu@gmail.com>
2019-07-22 05:48:52 -0700

    rewrote simple rating js

--- assets/javascripts/simple.rating.js
+++ assets/javascripts/simple.rating.js
@@ -1,71 +1,60 @@
-jQuery.fn.extend({
-  rating: function(options){
-    if(typeof(options)=='undefined') options={};
-    var ratings=[];
-    objs=this;
-    if(objs.length){
-      for(var i=0;i<objs.length;i++){
-        ratings.push(new simpleRating(options,objs[i]));
-      }
-    }
-  },
-});
 
-class simpleRating{
-  constructor(options,obj) {
-    this.obj=obj;
-    this.options=options;
-    this.rating=0;
-    this.init();
-  }
+function setstars(obj, rating){
 
-  init(){
-    var html='<div class="simple-rating star-rating">';
-    for(var i=0;i<5;i++){html+='<i class="fa fa-star-o" data-rating="'+(i+1)+'"></i>';}
-    html+='</div>';
-
-    $(this.obj)
-      .attr('type','hidden')
-      .after(html);
-
-    $(this.obj).next().children().click({classObj:this},function(e){
-      e.data.classObj.rate(this);
-    });
-
-    $(this.obj).next().children().mouseenter({classObj:this},function(e){
-      e.data.classObj.setstars($(this).data('rating'));
-    });
-
-    $(this.obj).next().children().mouseleave({classObj:this},function(e){
-      e.data.classObj.setstars(e.data.classObj.rating);
-    });
-
-    this.rating = $(this.obj).val();
-    this.setstars(this.rating);
-  }
-
-  rate(obj){
-    var rating=$(obj).data('rating');
-    $(obj).parent().prev().val(rating);
-    this.rating=rating;
-    this.refresh();
-  }
-
-  refresh(){
-    this.setstars(this.rating);
-  }
-
-  setstars(rating){
-    var stars=$(this.obj).next().children();
-    for(var i=0;i<5;i++){
-      var starObj=$(this.obj).next().children()[i];
-      if(i<rating){
-        $(starObj).removeClass('fa-star-o');
-        $(starObj).addClass('fa-star');
-      }else{
-        $(starObj).addClass('fa-star-o');
-        $(starObj).removeClass('fa-star');
-      }
+  var stars = $(obj).next().children();
+  for (var i=0; i<5; i++){
+    var starObj = $(obj).next().children()[i];
+    if (i < rating){
+      $(starObj).removeClass('fa-star-o');
+      $(starObj).addClass('fa-star');
+    } else {
+      $(starObj).removeClass('fa-star');
+      $(starObj).addClass('fa-star-o');
     }
   }
+
 }
+
+jQuery.fn.extend({
+
+  rating: function(options){
+
+    if(typeof(options)=='undefined') options={};
+
+    var objs = this;
+    if (objs.length){
+      for (var i=0; i<objs.length; i++){
+
+        var obj = objs[i];
+
+        var html='<div class="simple-rating star-rating">';
+        for(var j=0; j<5; j++)
+            html+='<i class="fa fa-star-o" data-rating="'+(j+1)+'"></i>';
+        html+='</div>';
+
+        $(obj).attr('type', 'hidden').after(html);
+
+        $(obj).next().children().on('click', function(e){
+          // todo: rate
+          var rating = $(this).data('rating');
+          $($(this).parent().prev()).val(rating); // set rating in input field
+          setstars($(this).parent().prev(), rating);
+        });
+
+        $(obj).next().children().on('mouseenter', function(e){
+          var rating = $(this).data('rating');
+          setstars($(this).parent().prev(), rating);
+        });
+
+        $(obj).next().children().on('mouseleave', function(e){
+          var rating = $(this).data('rating');
+          setstars($(this).parent().prev(), rating);
+        });
+
+        setstars(obj, $(obj).val());
+      }
+    }
+
+  },
+
+});
